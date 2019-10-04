a65da6a882d8399d4a5dd9db9335fa0da6adfa85
SagarMakhar <sagarmakhar@gmail.com>
2019-10-04 12:45:13 +0000

    RMX1901: Add initial device tree configuration files
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

---
+++ device.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Inherit from Realme sdm710-common
+$(call inherit-product, device/realme/sdm710-common/sdm710.mk)
+
+# Inherit from proprietary version
+$(call inherit-product-if-exists, vendor/realme/RMX1901/RMX1901-vendor.mk)
---
+++ lineage_RMX1901.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifer: Apache-2.0
+#
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from RMX1901 device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := RMX1901
+PRODUCT_NAME := lineage_RMX1901
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme X
+PRODUCT_MANUFACTURER := Realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20190809.034843 release-keys"
+
+BUILD_FINGERPRINT := "Realme/sdm710-user/sdm710-user:9/PKQ1.190101.001/eng.root.20190809.034843:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="RMX1901" \
+    TARGET_DEVICE="RMX1901"
