2d5bd97b32095d3e995524bb672af6a3c4f454f9
SagarMakhar <sagarmakhar@gmail.com>
2019-10-04 13:58:12 +0000

    RMX1901: Set LCD density prop
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,7 @@
+# Display density
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=480
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/system/etc/media_profiles_vendor.xml
