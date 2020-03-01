3254dcdb5f738460380b7fe52fd27c573c620497
SagarMakhar <sagarmakhar@gmail.com>
2020-03-01 08:00:32 +0000

    RMX1901: Commonize properties
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device-props.mk
+++
@@ -1,7 +0,0 @@
-# Display density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=420
-
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
